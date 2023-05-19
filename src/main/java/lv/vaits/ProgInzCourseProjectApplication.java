384ddc7f26979785df57040d6f8c41f47ed70017
unknown <katrina.sleze@gmail.com>
2023-05-19 10:55:23 +0300

    first project

---
+++ src/main/java/lv/vaits/ProgInzCourseProjectApplication.java
@@ -0,0 +1,13 @@
+package lv.vaits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgInzCourseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgInzCourseProjectApplication.class, args);
+	}
+
+}
